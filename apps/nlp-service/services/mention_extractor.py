6e06468ae77f6be9c82f640b0e193a871face5af
themaxence <maxence.ambert@etu.univ-savoie.fr>
2026-06-30 09:50:24 +0200

    fix: add PER label to SPACY_LABEL_MAP (fr/ru/de models use PER not PERSON)

--- apps/nlp-service/services/mention_extractor.py
+++ apps/nlp-service/services/mention_extractor.py
@@ -9,9 +9,11 @@ logger = structlog.get_logger()
 
 SPACY_LABEL_MAP = {
     "PERSON":        EntityType.PERSON,
+    "PER":           EntityType.PERSON,       # fr, ru, de, es, it, nl, ... models
     "ORG":           EntityType.ORGANIZATION,
     "GPE":           EntityType.LOCATION,
     "LOC":           EntityType.LOCATION,
+    "FAC":           EntityType.LOCATION,     # facilities (airports, bridges, …)
     "DATE":          EntityType.DATE,
     "TIME":          EntityType.DATE,
     "EMAIL":         EntityType.EMAIL,

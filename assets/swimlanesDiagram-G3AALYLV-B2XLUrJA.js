8d79ffaa6e5cc9d5ae9821f61426273716005aff
therealmaxence <maxence.ambert2005@gmail.com>
2026-07-20 13:44:04 +0000

    Deploying to gh-pages from @ therealmaxence/EntityGraph@7c4120e2a3aa480e7469c27740fea8e9fcbebadb 🚀

---
+++ assets/swimlanesDiagram-G3AALYLV-B2XLUrJA.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-23GEKE2U-B4Zx8JlR.js";import{_ as a}from"./index-rAmIz6qr.js";import"./chunk-5VM5RSS4-BftzbR7h.js";import"./chunk-XXDRQBXY-koGOESBP.js";import"./chunk-VR4S4FIN-u4Bdcccx.js";import"./chunk-32BRIVSS-CPNer2hq.js";import"./channel-DfURANpM.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};

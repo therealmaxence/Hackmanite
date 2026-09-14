a432f6d575625ed55d5ec601232e79af083261ea
therealmaxence <maxence.ambert2005@gmail.com>
2026-09-14 01:34:23 +0000

    Deploying to gh-pages from @ therealmaxence/Hackmanite@5a208fe61643d93b26df008d8d2873ef789f42ac 🚀

---
+++ assets/swimlanesDiagram-G3AALYLV-CrudRgz0.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-23GEKE2U-7wfpnwm_.js";import{_ as a}from"./index-fTBaEehq.js";import"./chunk-5VM5RSS4-BRUhKvU4.js";import"./chunk-XXDRQBXY-B9F4GbsP.js";import"./chunk-VR4S4FIN-DfUVuPud.js";import"./chunk-32BRIVSS-D53atmC6.js";import"./channel-C4JdF5cm.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
